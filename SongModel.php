3cf942194b58e462100ef965e045b94013e5449d
Aaron Foote <afoote@wesleyan.edu>
2023-11-13 23:58:55 -0500

    just a bit more tinkering

--- SongModel.php
+++ SongModel.php
@@ -26,13 +26,13 @@ class SongModel extends Database {
         $num = mysqli_num_rows($result) ;
         // User cannot rate the same song twice
         if ($num > 0){
-            echo "Cannot rate the same song twice" ;
+            echo json_encode("cannot rate the same song twice");
         } else {
             $sql2 = "INSERT INTO ratings (username,artist,song,rating) VALUES (?,?,?,?)";
             $stmt2 = $this->connection->prepare($sql2);
             $stmt2->bind_param('sssi',$username,$artist,$song,$rating);
             $stmt2->execute();
-            echo 'Song Added';
+            echo json_encode('song added');
         }
     }
     public function readSong($_ID){
